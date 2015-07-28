e6bc9490c5c93ec6fc3517a0ea9e5637d6843e0b
Tony Miller <mcfiredrill@gmail.com>
2015-07-28 10:48:37 +0900

    use :hackney.stream_body and plug's send_chunked
    also add logging

--- lib/proxy.ex
+++ lib/proxy.ex
@@ -1,8 +1,11 @@
+require Logger
+
 defmodule Proxy do
   use Plug.Builder
   import Plug.Conn
 
-  @target "http://google.com"
+  # @target "http://google.com"
+  @target "http://datafruits.streampusher.com:49236"
 
   plug Plug.Logger
   plug :dispatch
@@ -29,29 +32,37 @@ defmodule Proxy do
   defp write_proxy(conn, client) do
     # Check Plug.Conn.read_body/2 docs for maximum body value,
     # the size of each chunk, and supported timeout values.
-    case read_body(conn, []) do
+    case read_body(conn, [{:length, 100}]) do
       {:ok, body, conn} ->
+        Logger.info "reading body"
         :hackney.send_body(client, body)
+        Logger.info "sent body from ok"
         conn
       {:more, body, conn} ->
+        Logger.info "more body"
         :hackney.send_body(client, body)
+        Logger.info "sent body from more"
         write_proxy(conn, client)
     end
   end
 
   # Reads the client response and sends it back.
   defp read_proxy(conn, client) do
+    Logger.info "in read_proxy"
     {:ok, status, headers, client} = :hackney.start_response(client)
-    {:ok, body} = :hackney.body(client)
+    Logger.info "started response"
+    {:ok, body} = :hackney.stream_body(client)
+    Logger.info "body"
 
     # Delete the transfer encoding header. Ideally, we would read
     # if it is chunked or not and act accordingly to support streaming.
     #
     # We may also need to delete other headers in a proxy.
-    headers = List.keydelete(headers, "Transfer-Encoding", 1)
+    # headers = List.keydelete(headers, "Transfer-Encoding", 1)
 
-    %{conn | resp_headers: headers}
-    |> send_resp(status, body)
+    conn = send_chunked(conn, status)
+    {:ok, conn} = chunk(conn, body)
+    conn
   end
 
   defp uri(conn) do
